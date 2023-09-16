68445305352eb62d091d42a9ec33363ee1310fea
calvinludwig <calvin@gpssat.com.br>
2023-09-16 12:11:54 -0300

    feat: pacman aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,6 +1,16 @@
-alias cd="z"
+alias ..="cd .."
+alias ...= "cd ../.."
+alias ....= "cd ../../.."
 alias nv="nvim"
 alias ls="exa -l --icons --git"
+alias zjr="zellij run -c --"
+alias install="paru -S"
+alias pac="sudo pacman -S"
+alias pacu="sudo pacman -Syyuu"
+alias pacr="sudo pacman -Rns"
+alias pacc="sudo pacman -Scc"
+alias paclo="sudo pacman -Qdt"
+alias pacro="paclo && sudo pacman -Rns $(pacman -Qtdq)"
 
 # get fastest mirrors
 alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
--- zshrc.zsh
+++ zshrc.zsh
@@ -8,5 +8,7 @@ source ${ZDOTDIR:-$HOME}/plugins/zsh-vi-mode/zsh-vi-mode.plugin.zsh
 source ${ZDOTDIR:-$HOME}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 source ${ZDOTDIR:-$HOME}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
 eval "$(starship init zsh)"
 pfetch
