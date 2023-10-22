7c87fd52c9bc3bc3fa8ceb8ce987c2928ef73465
Calvin Ludwig <bludwig.calvin@gmail.com>
2023-10-22 20:20:37 -0300

    feat: pnpm

--- aliases.zsh
+++ aliases.zsh
@@ -1,22 +1,11 @@
 alias ..="cd .."
-alias ...= "cd ../.."
-alias ....= "cd ../../.."
 alias nv="nvim"
 alias ls="exa -l --icons --git"
 alias zjr="zellij run -c --"
-alias install="paru -S"
-alias pac="sudo pacman -S"
-alias pacu="sudo pacman -Syyuu"
-alias pacr="sudo pacman -Rns"
-alias pacc="sudo pacman -Scc"
-alias paclo="sudo pacman -Qdt"
-alias pacro="paclo && sudo pacman -Rns $(pacman -Qtdq)"
+alias pn="pnpm"
 
 # get fastest mirrors
-alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
-alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
-alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
-alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector -l 10 --sort rate --download-timeout 60 -a 6 --verbose --save /etc/pacman.d/mirrorlist"
 
 # confirm before overwriting something
 alias cp="cp -i"
--- exports.zsh
+++ exports.zsh
@@ -9,6 +9,17 @@ export BROWSER="brave"
 export MANPAGER='nvim +Man!'
 export MANWIDTH=999
 
+# pnpm
+export PNPM_HOME="/home/ludwig/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
+
+export GOPATH="$HOME/.go"
+export PATH="$PATH:$GOPATH/bin"
+
 eval "$(zoxide init zsh)"
 eval "$(fnm env --use-on-cd)"
 eval "$(zellij setup --generate-auto-start zsh)"
